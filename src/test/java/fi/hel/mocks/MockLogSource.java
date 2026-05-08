076ab871689fd1c4516ccc3206a115ac70f0cc60
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2026-05-08 09:56:45 +0300

    Use final where can and modern lambda syntax instead of older

--- src/test/java/fi/hel/mocks/MockLogSource.java
+++ src/test/java/fi/hel/mocks/MockLogSource.java
@@ -17,8 +17,8 @@ public class MockLogSource extends AbstractLogSource {
     private static List<Entry> entries = new ArrayList<>();
 
     public static class MockLogEntry implements AbstractLogSource.Entry {
-        private String id;
-        private AuditLogEvent event;
+        private final String id;
+        private final AuditLogEvent event;
         private boolean sent;
 
         public MockLogEntry(String id, AuditLogEvent event) {
@@ -70,11 +70,11 @@ public class MockLogSource extends AbstractLogSource {
     @Override
     public List<String> clearSentEntries(int retentionDays) {
         List<AbstractLogSource.Entry> toDelete = MockLogSource.entries.stream()
-                .filter(it -> it.isSent())
+                .filter(Entry::isSent)
                 .toList();
 
         List<String> ids = toDelete.stream()
-                .map(it -> it.getId())
+                .map(Entry::getId)
                 .toList();
 
         MockLogSource.entries = new ArrayList<>(MockLogSource.entries.stream()
